0696f40cf1d6248bc3e5238f9ac16988f52a78cd
JohannaPeanut <Johanna.k.michel@posteo.de>
2022-06-25 12:10:54 +0200

    modified models

--- server/models/bookmark.js
+++ server/models/bookmark.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
   user: {
-    type: mongoose.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'User'
   },
   sound: {
-    type: mongoose.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'Sound'
   },
