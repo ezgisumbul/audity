62812a4f40dd65ebeb218c3835b856fef7b4cc11
Martin <martin.kaumanns@diebrueder.com>
2022-06-25 12:24:49 +0200

    add sound model

---
+++ server/models/sound.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 36
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      maxlength: 400
+    }
+
+    tags: [
+      {
+        type: String,
+        enum: [
+          'city',
+          'street',
+          'traffic',
+          'capital',
+          'urban',
+          'animal',
+          'wind',
+          'water',
+          'weather',
+          'announces',
+          'loud',
+          'quiet',
+          'noisy',
+          'artificial',
+          'melodic',
+          'funnny',
+          'rare',
+          'nature',
+          'strange',
+          'voices',
+          'atmospheric',
+          'high pitched',
+          'echo',
+          'deep',
+          'pure'
+        ]
+      }
+    ],
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    postition: {
+      type: String,
+      default:'Point',
+      coordinates: [number]
+    },
+  
+    published: {
+      type: Boolean,
+      required: true
+    },
+
+    owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required,
+    },
+
+    soundFile: {
+      type: String,
+      required: true
+    },
+
+    quality: {
+      type: String,
+      enum: [
+       'high',
+        'medium',
+        'low'
+      ],
+      required: true
+    }
+    
+  },
+  { timestamps: true }
+);
+
+const Sound = mongoose.model('Sound', schema);
+
+module.exports = Sound;
