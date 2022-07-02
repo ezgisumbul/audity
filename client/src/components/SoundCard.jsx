a45ce5e7e5295dca4062a5887647f7720a63bfec
Martin <martin.kaumanns@diebrueder.com>
2022-07-02 10:44:17 +0200

    installed react-icon-package

--- client/src/components/SoundCard.jsx
+++ client/src/components/SoundCard.jsx
@@ -4,21 +4,25 @@ import './SoundCard.scss';
 const SoundCard = ({ sound }) => {
   return (
     <div className="sound-card">
-      <div>
-        <h2>{sound.title}</h2>
-        <small>
-          {sound.recordedAt &&
-            new Date(sound.recordedAt).toLocaleDateString('de-DE')}
-        </small>
-        <small>{sound.owner && sound.owner.name}</small>
-      </div>
-      <audio controls>
-        <source
-          src={sound.soundFile}
-          // type="mp3"
-        />
-      </audio>
-      <Link to={`/sound/${sound._id}`}>Details</Link>
+      {sound && (
+        <>
+          <div>
+            <h2>{sound.title}</h2>
+            <small>
+              {sound.recordedAt &&
+                new Date(sound.recordedAt).toLocaleDateString('de-DE')}
+            </small>
+            <small>{sound.owner && sound.owner.name}</small>
+          </div>
+          <audio controls>
+            <source
+              src={sound.soundFile}
+              // type="mp3"
+            />
+          </audio>
+          <Link to={`/sound/${sound._id}`}>Details</Link>
+        </>
+      )}
     </div>
   );
 };
