2dfeebe21b775ef0312ef8aed8f86311b79ec2cc
JohannaPeanut <Johanna.k.michel@posteo.de>
2022-07-03 20:43:07 +0200

    custom marker example, map styling unfinshed

--- client/src/components/SoundInputMap.jsx
+++ client/src/components/SoundInputMap.jsx
@@ -1,21 +1,22 @@
-import { Marker } from '@react-google-maps/api';
-import GenericMap from './GenericMap';
+import { Marker } from "@react-google-maps/api";
+import GenericMap from "./GenericMap";
 
 const SoundInputMap = ({ position, onPositionChange }) => {
   const handleSoundLocationSetting = (event) => {
     const lat = event.latLng.lat();
     const lng = event.latLng.lng();
 
-    onPositionChange({ type: 'Point', coordinates: [lat, lng] });
+    onPositionChange({ type: "Point", coordinates: [lat, lng] });
   };
 
   return (
     <GenericMap onClick={handleSoundLocationSetting}>
       {position && (
         <Marker
+          icon={"/Map-Marker-PNG-Pic.png"}
           position={{
             lat: position.coordinates[0],
-            lng: position.coordinates[1]
+            lng: position.coordinates[1],
           }}
         />
       )}
