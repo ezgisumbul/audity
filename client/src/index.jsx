c4d6624bda102a33286584d9102e610c35e26eeb
ezgisumbul <ezgisumbul@gmail.com>
2022-07-05 02:56:44 +0200

    start building custo player

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
+// import MyPlayer from './components/MyPlayer';
+
+// window.customElements.define('my-player', MyPlayer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
